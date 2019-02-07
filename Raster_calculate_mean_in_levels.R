f7604f3b555a9362877f21bc71db7ab76daece57
Thales Penha <41928323+tvpenha@users.noreply.github.com>
2019-02-07 17:38:31 -0200

    Delete Raster_calculate_mean_in_levels.R

--- Raster_calculate_mean_in_levels.R
+++
@@ -1,4 +0,0 @@
-# Function to calculate mean in a layer stacking raster
-# Repeat in a sequence of layers the function mean "n" times
-
-rMean <- calc( rasterfile , fun = function(x){ by(x , c( rep(seq(1:45), times = 31)) , mean, na.rm=TRUE ) }  )
